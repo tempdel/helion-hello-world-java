878cd4e4ab966ab93111ad19eca8aef4303d6543
Rajeev Pandey <rajeev.pandey@hp.com>
2015-04-06 16:21:02 -0700

    Update HelloServlet.java

--- src/main/java/org/hp/samples/HelloServlet.java
+++ src/main/java/org/hp/samples/HelloServlet.java
@@ -37,7 +37,7 @@ public class HelloServlet extends HttpServlet {
 		response.setContentType("text/plain");
 		response.setStatus(200);
 		PrintWriter writer = response.getWriter();
-		writer.println("Hello World, and me too");
+		writer.println("Hello World");
 		writer.close();
 	}
 }
