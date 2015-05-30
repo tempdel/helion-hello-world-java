df7ef1475278cf64e968f48415dba587e90e2c14
Rajeev Pandey <rajeev.pandey@hp.com>
2015-05-29 17:46:33 -0700

    Update HelloServlet.java

--- src/main/java/org/hp/samples/HelloServlet.java
+++ src/main/java/org/hp/samples/HelloServlet.java
@@ -44,7 +44,7 @@ ponse) throws ServletException, IOException {
 		response.setContentType("text/plain");
 		response.setStatus(200);
 		PrintWriter writer = response.getWriter();
-		writer.println("Hello World");
+		writer.println("Hello World and Discover 2015!!");
 		writer.close();
 		
 		HelloServlet demo = new HelloServlet("test");
